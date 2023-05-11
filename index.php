c3ab977b1d6fdb4723c1698a232e84f8b5083741
hyechong <ckgpwl2@gmail.com>
2023-05-11 11:51:06 +0900

    register class 세팅, 회원 테이블 생성 0511

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
+// print_r($_SERVER['REQUEST_URI']);
+
+$path_name = explode('/', $_SERVER['REQUEST_URI']);
+// print_r($path_name);
+// print_r($path_name[2]);
+
+if($path_name[2] == 'register' && $path_name[3] == 'signup'){
+  include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/signup.php';
+} else{
   echo 'index.php page!!';
+}
 
 ?>
